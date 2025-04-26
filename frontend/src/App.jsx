762b50d7631a0056d9869d19fb7e688d930817b7
GNANESH P <karshvarth2112@gmail.com>
2025-04-26 12:22:59 +0530

    Update app.py for accuracy and frontend App.jsx with recent changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://brain-tumor-detection-2-jd40.onrender.com/predict", {
         method: "POST",
         body: formData,
       });
